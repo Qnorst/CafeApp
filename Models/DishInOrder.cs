7a5bc5f32c24583708814b36ae39bb779ceb90fb
Денисенко Владимир Александрович <gr611_deval@ttit.local>
2022-04-26 18:47:17 +0700

    Cafe app created by me

---
+++ Models/DishInOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CafeApp
+{
+    public partial class DishInOrder
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int DishId { get; set; }
+        public int DishCount { get; set; }
+
+        public virtual Dish Dish { get; set; } = null!;
+        public virtual Order Order { get; set; } = null!;
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CafeApp
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            DishInOrders = new HashSet<DishInOrder>();
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime DataCreate { get; set; }
+
+        public virtual User User { get; set; } = null!;
+        public virtual ICollection<DishInOrder> DishInOrders { get; set; }
+    }
+}
